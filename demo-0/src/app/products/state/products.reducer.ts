1e6ec3f4b39c8a502944bca408af8c489110f554
gchilindrishvili <gchilindrishvili@credo.ge>
2024-07-10 04:36:24 +0400

    part of module 5 finished

---
+++ demo-0/src/app/products/state/products.reducer.ts
@@ -0,0 +1,18 @@
+import {createReducer, on} from "@ngrx/store";
+import {ProductsPageActions} from "./products.actions";
+
+export interface ProductsState {
+  showProductCode: boolean;
+}
+
+const initialState: ProductsState = {
+  showProductCode: true
+}
+
+export const productsReducer = createReducer(
+  initialState,
+  on(ProductsPageActions.toggleShowProductCode, (state) => ({
+    ...state,
+    showProductCode: !state.showProductCode
+  })),
+)
